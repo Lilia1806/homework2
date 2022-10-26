5d0e8b05fc0a05085522b9f0e64b617dc24500b9
Lilia1806 <liliataalaybekova@gmail.com>
2022-10-26 18:18:56 +0600

    ok

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,17 @@ public class Main {
         System.out.println(ageAndTemperature(13, 0));
     }
 
-    public static String ageAndTemperature(int age, double temperature) {
-        if (temperature > -19 && temperature < 31) {
+    public static String ageAndTemperature(int age, int temperature) {
+        if (age >= 20 && age <= 45 && temperature <= -20 && temperature >= 30) {
             return ("Можно идти гулять");
-        } else if (age <= 20)
-            if (temperature > 1 && temperature < 9) {
-                return ("Можно идти гулять");
-            } else if (age >= 46)
-                if (temperature > -11 && temperature < 25) {
-                    return ("Можно идти гулять");
-                } else {
-                    return ("Оставайтесь дома!");
-                }
-
-        return "";
+        } else if (age < 20 && temperature >= 0 && temperature <= 28) {
+            return ("Можно идти гулять");
+        } else if (age >= 45 && temperature >= -10 && temperature <= 25) {
+            return ("Можно идти гулять");
+        } else {
+            return ("Оставайтесь дома!");
+        }
     }
 }
+
+
